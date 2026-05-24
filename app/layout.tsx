ac3e30fe3f4e2831c0401b134c3f6f944b0ff107
thentrss-ship-it <thentrss@gmail.com>
2026-05-24 18:37:29 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "ГАЛАКТИКА",
+  description: "Оптовые поставки POD-систем и расходников",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
