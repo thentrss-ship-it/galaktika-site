d8f748b2df8cb7f40325868608615ae6e40a9f90
thentrss-ship-it <thentrss@gmail.com>
2026-05-25 18:22:02 +0300

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://galaxyopt.ru",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
