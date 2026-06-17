b60cba58053e72520cd28a1f8f1a49d88140406a
unknown <thentrss@gmail.com>
2026-06-17 22:35:55 +0300

    Fix robots sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://galaxyopt.ru";
+  const baseUrl = "https://www.galaxyopt.ru";
 
   return {
     rules: {
